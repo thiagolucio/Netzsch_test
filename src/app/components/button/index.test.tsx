52004cbd620411ab1ab28a18237f69cb0af50f72
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2024-02-18 20:02:29 -0300

    finish button test file

--- src/app/components/button/index.test.tsx
+++ src/app/components/button/index.test.tsx
@@ -5,24 +5,59 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './';
 
 describe('Button Component', () => {
-  test('renderiza o botão com a variante "primary"', () => {
-    render(<Button variant="primary" type="button">Clique Aqui</Button>);
-    const buttonElement = screen.getByRole('button', { name: /clique aqui/i });
+
+  const handleClick = jest.fn();
+  const children = "This is a Button"
+
+  test('Render Button with variant "default"', () => {
+    render(<Button variant="default" type="button">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('default');
+  });
+
+
+  test('Render Button with variant "primary"', () => {
+    render(<Button variant="primary" type="button">{children}</Button>);
+        const buttonElement = screen.getByRole('button', { name: children });
+
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveClass('primary');
   });
 
-  test('renderiza o botão com a variante "alert"', () => {
-    render(<Button variant="alert" type="button">Clique Aqui</Button>);
-    const buttonElement = screen.getByRole('button', { name: /clique aqui/i });
+  test('Render Button with variant "alert"', () => {
+    render(<Button variant="alert" type="button">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveClass('alert');
   });
 
-  test('chama a função onClick ao clicar no botão', () => {
-    const handleClick = jest.fn();
-    render(<Button onClick={handleClick} type="button" variant="default">Clique Aqui</Button>);
-    const buttonElement = screen.getByRole('button', { name: /clique aqui/i });
+
+  test('Render Button with variant "success"', () => {
+    render(<Button variant="success" type="button">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('success');
+  });
+
+  test('Render Button with variant "light"', () => {
+    render(<Button variant="light" type="button">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('light');
+  });
+
+
+  test('Render Button with variant "disabled"', () => {
+    render(<Button variant="disabled" type="button">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('disabled');
+  });
+
+  test('Callback onClick from button', () => {
+    render(<Button onClick={handleClick} type="button" variant="default">{children}</Button>);
+    const buttonElement = screen.getByRole('button', { name: children });
     fireEvent.click(buttonElement);
     expect(handleClick).toHaveBeenCalled();
   });
